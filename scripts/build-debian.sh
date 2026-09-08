43696c3bdf4a899ee5e8cf3f2c96d6e226189a3c
Goran Mekić <meka@sys.it.com>
2026-09-08 12:53:02 +0200

    Clean target directory before build

--- scripts/build-debian.sh
+++ scripts/build-debian.sh
@@ -150,6 +150,7 @@ if [[ -n "$TARGET_DIR" ]]; then
     echo "Using local target directory: $TARGET_DIR"
 fi
 
+cargo clean
 cargo build "${CARGO_ARGS[@]}"
 
 if [[ -n "$TARGET_DIR" ]]; then
--- scripts/build-fedora.sh
+++ scripts/build-fedora.sh
@@ -142,6 +142,7 @@ if [[ -n "$TARGET_DIR" ]]; then
     echo "Using local target directory: $TARGET_DIR"
 fi
 
+cargo clean
 cargo build "${CARGO_ARGS[@]}"
 
 if [[ -n "$TARGET_DIR" ]]; then
